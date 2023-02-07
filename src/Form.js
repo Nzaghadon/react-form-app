159878cf5115843516583026fe49fd3276046edc
Nzaghadon <nzaghaikechukwu@gmail.com>
2023-02-07 20:45:25 +0100

    added some styles

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,8 @@
+
 import { Checkbox, FormControlLabel, Radio } from '@mui/material';
+import { Button } from 'bootstrap';
 import { useForm } from 'react-hook-form'
+
 
 
 export default function Form() {
@@ -22,46 +25,47 @@ export default function Form() {
     });
     const onSubmit = (data) => console.log(data);
 
+
     return (
         <>
-            <div>
+            <div className='Form'>
                 <header><h1>Payment Form</h1></header><br />
                 <p>Complete this form to make your purchase.</p>
 
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <label>First name</label><br />
+                        <label htmlFor='firstName'>First name</label><br />
                         <input {...register("firstName", { required: true })}
                             aria-invalid={errors.firstName?.message} />
-                        {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
+                        {errors.firstName?.type === 'required' && <p className='warning' role="alert">First name is required</p>}
 
                     </div><br />
                     <div>
                         <label>Last name</label><br />
                         <input {...register("lastName", { required: true })}
                             aria-invalid={errors.lastName?.message} />
-                        {errors.lastName?.type === 'required' && <p role="alert">Last name is required</p>}
+                        {errors.lastName?.type === 'required' && <p className='warning' role="alert">Last name is required</p>}
                     </div><br />
                     <div>
                         <label>Username</label><br />
                         <input {...register("userName", { required: true })}
                             aria-invalid={errors.userName?.message} />
-                        {errors.userName?.type === 'required' && <p role="alert">Username is required</p>}
+                        {errors.userName?.type === 'required' && <p className='warning' role="alert">Username is required</p>}
                     </div><br />
                     <div>
                         <label>Email</label><br />
                         <input {...register("mail", { required: true, pattern: /^[a-z@.]/g })}
                             aria-invalid={errors.mail?.message} />
-                        {errors.mail?.type === 'pattern' && <p role="alert">This Email is not valid</p>}
-                        {errors.mail?.type === 'required' && <p role="alert">Email is required</p>}
+                        {errors.mail?.type === 'pattern' && <p className='warning' role="alert">This Email is not valid</p>}
+                        {errors.mail?.type === 'required' && <p className='warning' role="alert">Email is required</p>}
                     </div><br />
                     <div>
                         <label>Address</label><br />
                         <input {...register("address", { required: true })}
                             aria-invalid={errors.address?.message} />
-                        {errors.address?.type === 'required' && <p>Address is required</p>}
+                        {errors.address?.type === 'required' && <p className='warning' role="alert">Address is required</p>}
                     </div><br />
-                    <div>
+                    <div className='Country'>
                         <label>Country</label><br />
                         <select {...register("country", { required: true })}>
                             <option>U.S.A.</option>
@@ -77,10 +81,16 @@ export default function Form() {
                         <FormControlLabel control={<Checkbox />} label="Save this information for next time." />
                     </div><br /><hr />
                     <div>
-                        <p><h3>Payment</h3></p>
-                        <FormControlLabel value="creditCard" control={<Radio />} label="Credit card" />
+                        <h3><p>Payment</p></h3>
+                        {/*  <FormControlLabel value="creditCard" control={<Radio />} label="Credit card" />
                         <FormControlLabel value="debitCard" control={<Radio />} label="Debit card" />
-                        <FormControlLabel value="paypal" control={<Radio />} label="Paypal" />
+                        <FormControlLabel value="paypal" control={<Radio />} label="Paypal" /> */}
+                        <input type="radio" defaultChecked name='creditCard' />
+                        <label htmlFor="creditCard">Credit card</label>
+                        <input type="radio" name='debitCard' />
+                        <label htmlFor="debitCard">Debit card</label>
+                        <input type="radio" name='paypal' />
+                        <label htmlFor="paypal">Paypal</label>
                     </div><br />
                     <div>
                         <label>Name on card</label><br />
@@ -92,18 +102,19 @@ export default function Form() {
                     </div><br />
                     <div>
                         <label>Expiration</label><br />
-                        <input type="date" {...register("expiryDate")} /><br />
+                        <input type="date" {...register("expiryDate")} /><br /><br />
                         <label>CVV</label><br />
-                        <input {...register("cvv", { required: true })} />
+                        <input {...register("cvv", { required: true, pattern: /[0-9]/ })} />
+
                     </div><br /><hr />
 
-                    <input type="submit" />
+                    <input className='Submit' type="submit" />
 
 
 
-                </form>
+                </form >
 
-            </div>
+            </div >
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Form from './Form';
+import './Form.css';
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Form />);
+
 
 
 
